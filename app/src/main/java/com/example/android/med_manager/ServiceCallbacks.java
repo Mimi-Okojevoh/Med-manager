17f3289e76b82619cb275c7a164de0b2878f0c14
Mimi-Okojevoh <meejev@yahoo.com>
2018-04-18 01:39:55 -1100

    Initial commit

---
+++ app/src/main/java/com/example/android/med_manager/ServiceCallbacks.java
@@ -0,0 +1,8 @@
+package com.example.android.med_manager;
+
+
+
+
+interface ServiceCallBacks {
+    void updateUI();
+}
---
+++ app/src/main/java/com/example/android/med_manager/SettingsPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.med_manager;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SettingsPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_page);
+    }
+}
+
